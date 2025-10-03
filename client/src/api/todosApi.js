94f0ff4a9afa098c991d2149a42e3ef86e17f026
johnmartinroque <johnmartinroque@gmail.com>
2025-10-03 19:16:20 +0800

    delete todo frontend working

--- client/src/api/todosApi.js
+++ client/src/api/todosApi.js
@@ -2,12 +2,22 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/todos";
 
-export const getTodos = async () => {
+export const getTodosApi = async () => {
   try {
     const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
     console.error("Error fetching todo list", error);
     return [];
+  }
+};
+
+export const deleteTodoApi = async (id) => {
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting todo", error);
+    return null;
   }
 };
--- client/src/components/FetchTodo.jsx
+++ client/src/components/FetchTodo.jsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { getTodos } from "../api/todosApi";
+import { deleteTodoApi, getTodosApi } from "../api/todosApi";
 
 function FetchTodo() {
   const [todos, setTodos] = useState([]);
   const fetchTodo = async () => {
     try {
-      const todos = await getTodos();
+      const todos = await getTodosApi();
       setTodos(todos);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      await deleteTodoApi(id);
+      fetchTodo();
     } catch (err) {
       console.error(err);
     }
@@ -19,13 +28,21 @@ function FetchTodo() {
     <div className="text-white">
       FetchTodo from backend
       {todos.map((val) => (
-        <h3 key={val.id}>
-          <div className="flex flex-col items-center">
-            <div>TItLE: {val.title}</div>
-            <div>description: {val.description}</div>
-            <div>id: {val.id}</div>
+        <div className="mb-2 p-2 bg-gray-200 rounded">
+          <div className="bg-gray-500 p-2 rounded">
+            <div className="flex flex-col items-start space-y-1">
+              <div>Title: {val.title}</div>
+              <div>Description: {val.description}</div>
+              <div>ID: {val.id}</div>
+            </div>
+            <button
+              className="mt-2 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+              onClick={() => deleteTodo(val.id)}
+            >
+              Delete
+            </button>
           </div>
-        </h3>
+        </div>
       ))}
     </div>
   );
