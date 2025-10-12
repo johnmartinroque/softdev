d323c7e6f5919861b964624e04b344015b1fa9c9
johnmartinroque <johnmartinroque@gmail.com>
2025-10-12 11:26:34 +0800

    remove unused Products route from App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Products from "./pages/Products";
 import { Buffer } from "buffer";
 import process from "process";
 import Home from "./pages/Home";
@@ -16,7 +15,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
     </div>
